42a7d9194e9857c4bda5c77e83e6056286115db8
thebombadier <englandweluvu34@gmail.com>
2014-06-01 17:24:38 +0100

    Just a commit

--- delete_bot.py
+++ delete_bot.py
@@ -28,17 +28,17 @@ def read_list(var):
     f.close()
     
 def add_to_ban(id_p,badsubs):  
-	read_ban(badsubs)  
-	with open('ban.txt', 'a') as file:        
-		file.write(id_p + '\n')    
-	file.close()
-	if id_p + '\n' not in badsubs:
-		wiki = r.get_wiki_page('godwinbot','ban')
-		print wiki.content_md + "* **" + id_p + "**"
-		r.edit_wiki_page('godwinbot','ban',wiki.content_md + "\n\n* **" + id_p + "**",'I was banned from' + id_p)
-	else:
-		return "Already Banned"
-	
+    read_ban(badsubs)  
+    with open('ban.txt', 'a') as file:        
+        file.write(id_p + '\n')    
+    file.close()
+    if id_p + '\n' not in badsubs:
+        wiki = r.get_wiki_page('godwinbot','ban')
+        print wiki.content_md + "* **" + id_p + "**"
+        r.edit_wiki_page('godwinbot','ban',wiki.content_md + "\n\n* **" + id_p + "**",'I was banned from' + id_p)
+    else:
+        return "Already Banned"
+    
 r = praw.Reddit('delete negative karama comments for /u/godwin_finder by /u/the_bombadier')
 username = raw_input("Username: ")
 password = raw_input("Password: ")      
@@ -50,46 +50,46 @@ badsubs = []
 read_ban(badsubs)
 
 while True:
-	try:
-		for c in user.get_comments(limit=None):
-		    if time.time() - c.created_utc > 1800:
-			if c.score < 0 and (c.id + '\n' not in already_done or c.id not in already_done):
-			    c.delete()
-			    print 'Deleted Comment in subreddit: ' + c.subreddit.display_name + " score of " + str(c.score)
-			    perm = str(c.permalink) + " Score: " + str(c.score)
-			    add_to_delete(perm)
-			    already_done.append(c.id) 
-			    add_to_done(c.id)
-		unread = r.get_unread(limit=None)
-		for msg in unread:
-		    read_list(already_done)
-		    
-		    if msg.body.lower() == "delete":
-			
-			bot_comment_id = msg.parent_id
-			bot_comment = r.get_info(thing_id=bot_comment_id)
-			person_comment = r.get_info(thing_id=bot_comment.parent_id)
-			if msg.author.name == person_comment.author.name and bot_comment.id + '\n' not in already_done:
-			    bot_comment.delete()
-			    print "Deleted comment because " + person_comment.author.name + " replied with delete "
-			    perm = str(bot_comment.permalink) + " User " + person_comment.author.name + " replied with delete"    
-			    add_to_delete(perm)
-			    already_done.append(bot_comment.id) 
-			    add_to_done(bot_comment.id)
-		    if "you have been banned from posting to " in msg.body.lower():
-			
-				if "/r/" + msg.subreddit.display_name  in msg.body.lower() and msg.subreddit.display_name + '\n' not in badsubs and msg.author == None:
-					sub = r.get_subreddit(str(msg.subreddit.display_name))
-					print "Baned from: " + msg.subreddit.display_name
-					add_to_ban(str(msg.subreddit.display_name),badsubs)
-					badsubs.append(msg.subreddit.display_name)	
-					time.sleep(60)	
-			    
+    try:
+        for c in user.get_comments(limit=None):
+            if time.time() - c.created_utc > 1800:
+                if c.score < 0 and (c.id + '\n' not in already_done or c.id not in already_done):
+                    c.delete()
+                    print 'Deleted Comment in subreddit: ' + c.subreddit.display_name + " score of " + str(c.score)
+                    perm = str(c.permalink) + " Score: " + str(c.score)
+                    add_to_delete(perm)
+                    already_done.append(c.id) 
+                    add_to_done(c.id)
+        unread = r.get_unread(limit=None)
+        for msg in unread:
+            read_list(already_done)
+            
+            if msg.body.lower() == "delete":
+            
+                bot_comment_id = msg.parent_id
+                bot_comment = r.get_info(thing_id=bot_comment_id)
+                person_comment = r.get_info(thing_id=bot_comment.parent_id)
+                if msg.author.name == person_comment.author.name and bot_comment.id + '\n' not in already_done:
+                    bot_comment.delete()
+                    print "Deleted comment because " + person_comment.author.name + " replied with delete "
+                    perm = str(bot_comment.permalink) + " User " + person_comment.author.name + " replied with delete"    
+                    add_to_delete(perm)
+                    already_done.append(bot_comment.id) 
+                    add_to_done(bot_comment.id)
+            if "you have been banned from posting to " in msg.body.lower():
+            
+                if "/r/" + msg.subreddit.display_name  in msg.body.lower() and msg.subreddit.display_name + '\n' not in badsubs and msg.author == None:
+                    sub = r.get_subreddit(str(msg.subreddit.display_name))
+                    print "Baned from: " + msg.subreddit.display_name
+                    add_to_ban(str(msg.subreddit.display_name),badsubs)
+                    badsubs.append(msg.subreddit.display_name)  
+                    time.sleep(60)  
+                
                             
                      
-	except:
-	     print "Unexpected error:", traceback.format_exc()
-	     add_to_error(traceback.format_exc())
-	     time.sleep(500)
+    except:
+         print "Unexpected error:", traceback.format_exc()
+         add_to_error(traceback.format_exc())
+         time.sleep(500)
      
 
--- godwin.py
+++ godwin.py
@@ -10,10 +10,10 @@ def login():
     name = raw_input("Username: ")
     password = raw_input("Password: ")
     try:
-	r.login(name,password)
-	return "Logged in as: " + name
+        r.login(name,password)
+        return "Logged in as: " + name
     except:
-	return "Error"
+        return "Error"
 
 def a(test_str):
     ret = ''
@@ -35,139 +35,144 @@ def a(test_str):
     
 def add_to_error(id_p):
     with open('error.txt', 'a') as file:
-	file.write(id_p + '\n')
+        file.write(id_p + '\n')
     file.close()
 
 def add_to_list(id_p):
     with open('posts.txt', 'a') as file:
-	file.write(id_p + '\n')
+        file.write(id_p + '\n')
     file.close()
 
 def read_ban(var):
     f = open('ban.txt', 'r')
     for line in f:
-	var.append(line)
+        var.append(line)
     f.close()
 
 def add_to_ban(id_p,badsubs):  
     read_ban(badsubs)  
-    with open('ban.txt', 'a') as file:	      
-	file.write(id_p + '\n')	   
-    file.close()
+    with open('ban.txt', 'a') as file:        
+       file.write(id_p + '\n')    
+       file.close()
     if id_p + '\n' not in badsubs:
-	wiki = r.get_wiki_page('godwinbot','ban')
-	print wiki.content_md + "* **" + id_p + "**"
-	r.edit_wiki_page('godwinbot','ban',wiki.content_md + "\n\n* **" + id_p + "**",'I was banned from' + id_p)
-	time.sleep(60) 
+        wiki = r.get_wiki_page('godwinbot','ban')
+        print wiki.content_md + "* **" + id_p + "**"
+        if "* **" + id_p + "**" not in wiki.content_md:
+            r.edit_wiki_page('godwinbot','ban',wiki.content_md + "\n\n* **" + id_p + "**",'I was banned from' + id_p)
+        else:
+            print "Actually, hold on... there's an error in ban.txt but wiki already editted, ban.txt updated"
+            
+        time.sleep(60)
+        
     else:
-	return "Already Banned"
+       return "Already Banned"
     
 def read_list(var):
     f = open('posts.txt', 'r')
     for line in f:
-	var.append(line)
+        var.append(line)
     f.close()
 
 
 
 def handle_ratelimit(func, *args, **kwargs):
     while True:
-	try:
-	    func(*args, **kwargs)
-	    break
-	except praw.errors.RateLimitExceeded as error:
-	    print '\tSleeping for %d seconds' % error.sleep_time
-	    time.sleep(error.sleep_time)
+        try:
+            func(*args, **kwargs)
+            break
+        except praw.errors.RateLimitExceeded as error:
+            print '\tSleeping for %d seconds' % error.sleep_time
+            time.sleep(error.sleep_time)
     
 def contains(selftext,words,sub_id,submission,done):
-	badsubs = []
-	read_ban(badsubs)
-	some = 0
-	read_list(done)
-	global amount
-	global nazi_co
-	amount = 0
-	com_word = ""
-	nazi_co = 0
-	global sub_post
-	global time_com
-	global time_sub
-	global com_body
-	global com_name
-	global com
-	nazi = ["holocaust","jews","nazi","hitler","ww2","racism","antisemitism","ethnic","cleansing","semitism"]
-	if sub_id + '\n' not in done and str(submission.subreddit) not in badsubs and len(selftext)< 4000:
-		has_nazi_text = any(string in selftext.lower() for string in words)
-		has_nazi_title = any(string in submission.title.lower() for string in nazi)
-		add_to_list(submission.id)
-		done.append(submission.id)
-		if has_nazi_text or has_nazi_title:
-			some = 0
+    badsubs = []
+    read_ban(badsubs)
+    some = 0
+    read_list(done)
+    global amount
+    global nazi_co
+    amount = 0
+    com_word = ""
+    nazi_co = 0
+    global sub_post
+    global time_com
+    global time_sub
+    global com_body
+    global com_name
+    global com
+    nazi = ["holocaust","jews","nazi","hitler","ww2","racism","antisemitism","ethnic","cleansing","semitism"]
+    if sub_id + '\n' not in done and str(submission.subreddit) not in badsubs and len(selftext)< 4000:
+        has_nazi_text = any(string in selftext.lower() for string in words)
+        has_nazi_title = any(string in submission.title.lower() for string in nazi)
+        add_to_list(submission.id)
+        done.append(submission.id)
+        if has_nazi_text or has_nazi_title:
+            some = 0
 
-			return "Nazi referenced in submission: " + submission.title + " so no comment made " 
-	    
-	    
-		flat = praw.helpers.flatten_tree(submission.comments)
-		for comment in flat:
-			amount = amount + 1
-			for nazi in words:
-				if not hasattr(comment, 'body'):
-					continue
-				if nazi in repr(comment.body.lower()):
-					nazi_co = nazi_co + 1
-					if nazi_co == 1:
-							sub_post = submission.subreddit
-							time_com = comment.created_utc
-							time_sub = submission.created_utc
-							com_body = comment.body
-							com_name = comment.author.name
-							com = comment
-		if nazi_co < 5 and nazi_co > 0:
-			print"money"
-			some = 1
-			
-			if amount > 1:
-				com_word = " comments"
-				print com_word
-			else:
-				com_word = " comment"
-				print com_word
-			try:
-				print "why"
-				time1 =	 time_com - time_sub
-				com.reply("It took this thread " + datetime.datetime.fromtimestamp(time1).strftime("%H hours, %M minutes, %S seconds") +  " and " + str(amount) + str(com_word) + " to make a reference to the nazis, for more information look up [Godwin's Law] (http://en.wikipedia.org/wiki/Godwin's_law). \n *** \n  *^[about](http://www.reddit.com/r/godwinbot/wiki/index) ^| ^[source](https://www.github.com/thebombadier/godwinbot) ^| ^/u/" + str(com_name) + " ^can ^reply ^with ^'delete' ^to ^delete ^this ^comment. ^Additionally, ^if ^this ^gets ^a ^score ^of ^-1 ^after ^30 ^minutes ^this ^comment ^will ^be ^deleted.*" )
-				
-				
-				return "Nazi reference in comment by " + com_name + " Comment: " + com_body
-				time.sleep(300)
-				
-			except praw.errors.RateLimitExceeded as e:
-				print 'Sleeping for ' + str(e.sleep_time) + ' seconds'
-				time.sleep(e.sleep_time)
-			except Exception as e:
-				add_to_error("REPLY FAILED: %s @ %s"%(e,sub_post))
-				if str(e) == '403 Client Error: Forbidden':
+            return "Nazi referenced in submission: " + submission.title + " so no comment made " 
+        
+        
+        flat = praw.helpers.flatten_tree(submission.comments)
+        for comment in flat:
+            amount = amount + 1
+            for nazi in words:
+                if not hasattr(comment, 'body'):
+                    continue
+                if nazi in repr(comment.body.lower()):
+                    nazi_co = nazi_co + 1
+                    if nazi_co == 1:
+                            sub_post = submission.subreddit
+                            time_com = comment.created_utc
+                            time_sub = submission.created_utc
+                            com_body = comment.body
+                            com_name = comment.author.name
+                            com = comment
+        if nazi_co < 5 and nazi_co > 0:
+            print"Hold on I've found something !"
+            some = 1
+            
+            if amount > 1:
+                com_word = " comments"
+                
+            else:
+                com_word = " comment"
+                
+            try:
+                
+                time1 =  time_com - time_sub
+                com.reply("It took this thread " + datetime.datetime.fromtimestamp(time1).strftime("%H hours, %M minutes, %S seconds") +  " and " + str(amount) + str(com_word) + " to make a reference to the Nazis, for more information look up [Godwin's Law] (http://en.wikipedia.org/wiki/Godwin's_law). \n *** \n  *^[about](http://www.reddit.com/r/godwinbot/wiki/index) ^| ^[source](https://www.github.com/thebombadier/godwinbot) ^| ^/u/" + str(com_name) + " ^can ^reply ^with ^'delete' ^to ^delete ^this ^comment. ^Additionally, ^if ^this ^gets ^a ^score ^of ^-1 ^after ^30 ^minutes ^this ^comment ^will ^be ^deleted.*" )
+                
+                
+                return "Nazi reference in comment by " + com_name + " in subreddit: " + sub_post + " Comment: " + com_body
+                time.sleep(300)
+                
+            except praw.errors.RateLimitExceeded as e:
+                print 'Sleeping for ' + str(e.sleep_time) + ' seconds'
+                time.sleep(e.sleep_time)
+            except Exception as e:
+                add_to_error("REPLY FAILED: %s @ %s"%(e,sub_post))
+                if str(e) == '403 Client Error: Forbidden':
 
-					add_to_ban(str(sub_post),badsubs)
-					badsubs.append(str(sub_post))
+                    add_to_ban(str(sub_post),badsubs)
+                    badsubs.append(str(sub_post))
 
-					return "Banned from this subreddit"
-			except Exception as e:
-				add_to_error(e)
-				return "wtf"
-				return str(e)
-		elif nazi_co > 5:
-			return "Too many comments about nazis"		    
-	if some == 0:
-		return "No Nazi reference found"
+                    return "Banned from this subreddit"
+            except Exception as e:
+                add_to_error(e)
+                return "wtf"
+                return str(e)
+        elif nazi_co > 5:
+            return "Too many comments about nazis"          
+    if some == 0:
+        return "No Nazi reference found"
     
-	
-	
+    
+    
 
-	    
+        
 r = praw.Reddit("Godwin's Law bot by /u/the_bombadier"
-		"http://www.github.com/thebombadier/godwinbot"
-		)
+        "http://www.github.com/thebombadier/godwinbot"
+        )
 
 
 
@@ -178,17 +183,17 @@ print login()
 
 while True:
     try:
-	words = ["nazi", "hitler"]
-	done = []
-	read_list(done)
-	number = 1
-	for comment in praw.helpers.comment_stream(r,'all', limit = None):
-	    if comment.submission.id + '\n' not in done:
-		post = comment.submission
-		selftext = post.selftext.lower()
-	    
-		print contains(selftext,words,post.id,post,done)
+        words = ["nazi", "hitler"]
+        done = []
+        read_list(done)
+        number = 1
+        for comment in praw.helpers.comment_stream(r,'all', limit = None):
+            if comment.submission.id + '\n' not in done:
+                post = comment.submission
+                selftext = post.selftext.lower()
+            
+            print contains(selftext,words,post.id,post,done)
     except:
-	 print "Unexpected error:", traceback.format_exc()
-	 add_to_error(traceback.format_exc())
-	 time.sleep(500)
+     print "Unexpected error:", traceback.format_exc()
+     add_to_error(traceback.format_exc())
+     time.sleep(500)
